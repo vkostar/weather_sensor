52e3eb088d9df98b18109bb997ea00c5f1744e03
”konstantin <vihroff@gmail.com>
2022-07-03 00:48:08 +0300

    Initial commit

---
+++ src/test/java/com/kostar/weather_sensor/WeatherSencorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kostar.weather_sensor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherSencorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
